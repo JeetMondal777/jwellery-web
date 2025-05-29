0c18444088e4bed4c777f221f55b120617db1910
Jeet Mondal <jeetmn0226@gmail.com>
2025-05-29 13:03:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,29 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const express = require('express');
-const cookieParser = require("cookie-parser");
-
-const connectDB = require('./db/db');
-connectDB();
-const productRoutes = require('./routes/product.routes');
-const userRoutes = require('./routes/user.routes');
 const app = express();
+
 const cors = require('cors');
-
 app.disable("x-powered-by");
-
-
-app.use(cookieParser());
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],}));
+
+const cookieParser = require("cookie-parser");
+
+const connectDB = require('./db/db');
+connectDB();
+const userRoutes = require('./routes/user.routes');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
